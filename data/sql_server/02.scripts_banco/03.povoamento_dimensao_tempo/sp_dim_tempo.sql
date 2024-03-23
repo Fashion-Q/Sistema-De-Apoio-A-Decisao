88c7b19332dabe2f5b30d4a8a6b40e1dc14ea48f
ValdirMS <59018974+ValdirMS@users.noreply.github.com>
2024-03-23 11:16:03 -0300

    Update sp_dim_tempo.sql

--- data/sql_server/02.scripts_banco/03.povoamento_dimensao_tempo/sp_dim_tempo.sql
+++ data/sql_server/02.scripts_banco/03.povoamento_dimensao_tempo/sp_dim_tempo.sql
@@ -1,49 +1,93 @@
 -- Script para povoar a dimensão tempo
-use dw_lowlatency
-create or alter procedure sp_dim_tempo (@dt_inicial datetime, @dt_final datetime)
-as
-begin
-    set nocount on
-    set language brazilian
-    while (@dt_inicial < @dt_final)
-	begin
-	   if exists (select 1 from dim_tempo 
-	              where data = @dt_inicial)
-		   continue
-	   insert into dim_tempo
-	   select 
-	         data = @dt_inicial,
-			 dia = datepart(dd,@dt_inicial),
-			 dia_semana = datename(dw,@dt_inicial),
-			 fim_semana = case 
-			    when datepart(dw,@dt_inicial) in (1,7) THEN 'SIM'
-			    else 'NAO'
-			 end,
-			 mes = datepart(mm,@dt_inicial),
-			 nome_mes = datename(Month,@dt_inicial),
-			 trimestre = datepart(qq, @dt_inicial),
-			 nome_trimestre = trim(str(datepart(qq,@dt_inicial))) + 'º Trimestre/' +
-                              trim(str(datepart(yy,@dt_inicial))),
-			 semestre = case 
-			            when datepart(mm,@dt_inicial) <=6 THEN 1
-				          else 2
-			            end,
-			 nome_semestre = case
-			                  when datepart(mm,@dt_inicial) <=6 THEN
-				                   '1º Semestre/' +  trim(str(datepart(yy,@dt_inicial)))
-				              else '2º Semestre/' +  trim(str(datepart(yy,@dt_inicial)))
-			                  end,
-			 ano = datepart(yy,@dt_inicial)
-			 
-			 set @dt_inicial = @dt_inicial + 1
-	end
-end
+use hawkmart
+	
+CREATE OR ALTER PROCEDURE sp_dim_tempo @DataInicial DATE, @DataFinal DATE AS
+BEGIN
+	SET NOCOUNT ON 
+	PRINT 'Data Inicial: ' + convert(varchar(10),@DataInicial,103)
+	PRINT 'Data Final: ' + convert(varchar(10),@DataFinal,103)
+	DECLARE @EhFimDeSemana varchar(3)
+	while (@DataInicial < @DataFinal)
+	BEGIN
+		IF (DATEPART(WEEKDAY,@dataInicial) IN (1,7))
+			SET @EhFimDeSemana = 'SIM'
+		else
+			SET @EhFimDeSemana = 'NAO'
 
--- Testes
+		INSERT INTO DIM_TEMPO(
+			NIVEL,
+			DATA,
+			DIA,
+			DIA_SEMANA,
+			FIM_SEMANA,
+			FERIADO,
+			FL_FERIADO,
+			MES,
+			NOME_MES,
+			TRIMESTRE,
+			NOME_TRIMESTRE,
+			SEMESTRE,
+			NOME_SEMESTRE,
+			ANO
+		)
+		VALUES(
+			'DIA',
+			@DataInicial,
+			DAY(@DataInicial),
+			DATENAME(WEEKDAY, @DataInicial),
+			@EhFimDeSemana,
+			NULL,
+			'NAO',
+			MONTH(@DataInicial),
+			DATENAME(MONTH,@DataInicial),
+			(MONTH(@DataInicial) -1) / 3 + 1,
+			'NOME_TRIMESTRE',
+			(MONTH(@DataInicial) -1) / 6 + 1,
+			'NOME_SEMESTRE',
+			YEAR(@DataInicial)
+		)
+		SET @DataInicial = DATEADD(DAY,1,@DataInicial)
+	END
+END
 
--- truncate table dim_tempo
-
-exec sp_dim_tempo '20230102', '20230131'
-truncate table dim_tempo
+EXEC sp_dim_tempo '2024-01-01','2025-01-01'
 
 select * from dim_tempo
+truncate table dim_tempo
+
+CREATE TABLE DIM_FERIADOS (
+	ID_FERIADO INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	DATA DATETIME NOT NULL,
+	DESCRICAO VARCHAR(100) NOT NULL,
+	TIPO VARCHAR(50) NOT NULL CHECK (TIPO in
+	('NACIONAL','ESTADUAL')),
+	CONSTRAINT UK_FERIADO UNIQUE (DATA)
+)
+truncate table DIM_FERIADOS
+select * from DIM_FERIADOS
+INSERT INTO DIM_FERIADOS(DATA,DESCRICAO,TIPO)VALUES
+('2024-01-01','Confraternização Universal','NACIONAL'),
+('2024-12-02','Carnaval','NACIONAL'),
+('2024-13-02','Carnaval','NACIONAL'),
+('2024-23-03','Paixão de Cristo','NACIONAL'),
+('2024-21-04','Tiradentes','NACIONAL'),
+('2024-01-05','Dia do Trabalho','NACIONAL'),
+('2024-30-05','Corpus Christi','NACIONAL'),
+('2024-07-09','Independência do Brasil','NACIONAL'),
+('2024-12-10','Nossa Sr. Aparecida - Padroeira do Brasil','NACIONAL'),
+('2024-02-11','Finados','NACIONAL'),
+('2024-15-11','Proclamação da República','NACIONAL'),
+('2024-20-11','Dia Nacional de Zumbi e da Consciência Negra','NACIONAL'),
+('2024-25-12','Natal','NACIONAL')
+
+
+CREATE OR ALTER PROCEDURE so_atualiza_feriado @ANO INT AS
+BEGIN
+	print 'hello: ' + CAST(@ANO AS VARCHAR(4))
+	update DIM_TEMPO SET FL_FERIADO = 'SIM', FERIADO = f.DESCRICAO
+	from DIM_TEMPO t
+	inner join DIM_FERIADOS f on (t.DATA = f.DATA)
+	where t.ANO = @ANO
+END
+
+EXEC so_atualiza_feriado 2024
